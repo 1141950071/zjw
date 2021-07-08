6dde0d189c8e9558c61b272e49df7b5476fc445d
zjw <963755885@qq.com>
2021-07-08 15:18:48 +0800

    全新项目

--- src/git/Teacher.java
+++ src/git/Teacher.java
@@ -1,4 +1,39 @@
 package git;
 
 public class Teacher {
+    private int id;
+    private String name;
+    private String age;
+    public Teacher() {
+    }
+
+    public Teacher(int id, String name, String age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
